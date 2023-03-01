7942fe77cda1385ddb9460c48eefbe41912b68e3
Thut Thamrongnawasawat <thutthamrongnawasawat@Thuts-MacBook-Air.local>
2023-03-01 21:18:20 +0700

    feat:first edition, working on scaffold

---
+++ src/components/Question-1.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-gray-800">
+    <form>
+      <h1 class="text-yellow-200 text-3xl">First dummy question</h1>
+    </form>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      'color':{
+
+      },
+      'animation': {
+        'text': 'text 5s ease infinite',
+      },
+      'keyframes': {
+        'text': {
+          '0%, 100%': {
+            'background': 'linear-gradient(90deg, #a855f7 0%,#ef4444  50%, #eab308 100%);'
+          },
+          '50%': {
+            
+            'background': 'linear-gradient(90deg, #eab308  0%,  50%,#a855f7 100%);'
+          }
+        },
+      }
+    },
+  },
+  
+}
